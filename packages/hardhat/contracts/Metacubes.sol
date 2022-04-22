5249d20d4677cf83f44a9e441443acb61ccbb4cd
Sam Gbafa <skgbafa@gmail.com>
2022-04-22 23:28:23 +0200

    pseudocode

--- packages/hardhat/contracts/Metacubes.sol
+++ packages/hardhat/contracts/Metacubes.sol
@@ -11,13 +11,16 @@ contract Metacubes is ERC721, ERC721Burnable, Ownable {
   using Counters for Counters.Counter;
 
   Counters.Counter private _tokenIdCounter;
+  Counters.Counter public votesToStart;
 
   bool public contestReady = false;
   bool public contestStarted = false;
   bool public refundMode = false;
+  address public contestWinner;
 
   string private hashedPassword;
 
+  mapping(uint256 => bool) private _voteRecord;
 
   constructor(string memory _hashedPassword) ERC721("metacubes", "CUBES") {
     hashedPassword = _hashedPassword;
@@ -47,22 +50,38 @@ contract Metacubes is ERC721, ERC721Burnable, Ownable {
       return refundMode;
   }
 
-  function guessPassword() public returns (uint256) {
+  function guessPassword(uint256 tokenID, string memory password) public returns (uint256) {
       // TODO: implement
-
+      // require contest occuring
+      // require caller is owner of tokenid
+      // require payment of 0.01 ether
+      // require(hashedPassword == keccak256(password));
+      
+      // correct guess!
+      // set contest over
+      // transfer eth
       return 0;
   }
 
   function voteToStartContest() public {
       // TODO: implement
+      // require contest ready
+      // require _voteRecord[token] false
+      // increment votesToStart
+      // _voteRecord[token] = true
   }
 
   function startContest() public {
       // TODO: implement
+      // require contest ready
+      // require votesToStart > quorum
+      // require not refund mode
+      
+      // set contest started
   }
 
   function toggleContestReady() public onlyOwner {
-      contestReady = !contestReady;
+    contestReady = !contestReady;
   }
 
   function toggleRefundMode() public onlyOwner {
@@ -71,6 +90,9 @@ contract Metacubes is ERC721, ERC721Burnable, Ownable {
 
   function refundToken() public onlyOwner {
       // TODO: implement
+      // require refund mode on
+      // burn token
+      // return mint fee to message.sender
   }
   
 }
